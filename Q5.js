0875352e3388a862ced1669bc81f22205a0d3cd3
Sanjay-2001 <90711694+Sanjay-2001@users.noreply.github.com>
2021-12-25 22:22:31 +0530

    Add files via upload

---
+++ Q5.js
@@ -0,0 +1,11 @@
+// Check whether the number is even or odd
+let a=0;
+if(a==0){
+    console.log("The number is neither odd nor even.");
+}
+else if(a%2==0){
+    console.log("The number is even.");
+}
+else{
+    console.log("The number is odd.");
+}
